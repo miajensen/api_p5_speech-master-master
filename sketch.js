c3133cef9fdaa95ff342af3358c3cbf45caa98e7
Mia <47887813+miajensen@users.noreply.github.com>
2019-03-01 23:16:47 +0100

    Ændret hvad der kommer frem

--- sketch.js
+++ sketch.js
@@ -4,9 +4,16 @@ let resultP;
 let leftDiv;
 let counter;
 let cnv, myRec, btn, txt;
+let img, img_2, img_3, img_4, img_5;
 
 function setup() {
     //Jeg sætter en kommentar her 
+    img = loadImage('image/kakao.jpg');
+    img_2 = loadImage('image/skumfiduser.jpg');
+    img_3 = loadImage('image/is.jpg');
+    img_4 = loadImage('image/vinter.jpg');
+    img_5 = loadImage('image/film.jpg');
+
     let SpeechRecognition = window.webkitSpeechRecognition ||
         window.mozSpeechRecognition ||
         window.msSpeechRecognition ||
@@ -40,14 +47,29 @@ function setup() {
     }
 }
 
-function draw() {}
+function draw() {
+}
 
 function showResult() {
     if (myRec.resultValue == true) {
         sentence = myRec.resultString;
         resultP.html(sentence);
 
-        if (sentence.includes("orange")) {}
+        if (sentence.includes("kakao")) {
+            image(img, 0, height / 2, img.width / 9, img.height / 8);
+        }
+        if (sentence.includes("skumfiduser")) {
+            image(img_2, 0, height / 2, img.width / 9, img.height / 8);
+        }
+        if (sentence.includes("is")) {
+            image(img_3, 0, height / 2, img.width / 9, img.height / 8);
+        }
+        if (sentence.includes("vinter")) {
+            image(img_4, 0, height / 2, img.width / 9, img.height / 8);
+        }
+        if (sentence.includes("film")) {
+            image(img_5, 0, height / 2, img.width / 9, img.height / 8);
+        }
 
     }
 }
